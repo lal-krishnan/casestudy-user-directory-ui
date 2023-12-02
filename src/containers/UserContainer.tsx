2453d69f354372f0ae606bb42fa8edc6ca0564cd
Lal krishnan <007lalkrishnansl@gmail.com>
2023-12-03 01:24:03 +0530

    Change URL and style

--- src/containers/UserContainer.tsx
+++ src/containers/UserContainer.tsx
@@ -46,7 +46,7 @@ const UserContainer: React.FC = () => {
                     <h2>User List</h2>
                 </Grid>
                 <Grid sm={4} item style={{ textAlign: "right", paddingTop: "20px" }}>
-                    <Button variant="outlined" color="success" onClick={() => setOpenDrawer(true)} >New User</Button>
+                    <Button variant="outlined" color="primary" onClick={() => setOpenDrawer(true)} >New User</Button>
                 </Grid>
             </Grid>
             <UserGrid doFetchUser={doFetchUser} handleEdit={handleEdit} showNotification={showNotification} />
